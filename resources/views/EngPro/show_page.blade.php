a398fe52dc8658ce70a5407e1063a6a61af3c0cc
fmujie <1935377905@qq.com>
2020-11-29 21:40:02 +0800

    add images&video

--- resources/views/EngPro/show_page.blade.php
+++ resources/views/EngPro/show_page.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>show page</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-  {{-- <link rel="stylesheet" href="{{ asset('css/yanHua.css') }}"> --}}
-  <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
-  <script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>show page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    {{-- <link rel="stylesheet" href="{{ asset('css/yanHua.css') }}"> --}}
+    <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> --}}
+    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> --}}
+    {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script> --}}
+    {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> --}}
   <style>
     #introContain::after{
         content: '|';
@@ -91,6 +96,11 @@
         background-color: #555;
     }
     /* 滚动条样式设置 */
+    .carousel-inner div {
+        height: 200px;
+        background-repeat: no-repeat;
+        background-position: center center;
+    }
   </style>
 </head>
 <body>
@@ -1152,7 +1162,42 @@
               </div>
             </div>
           </div>
-        
+          <div id="carouselExampleIndicators" class="carousel slide mt-3" data-ride="carousel" style="height: 200px;">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" style="height: 200px;">
+              <div class="carousel-item active" style="background-image: url({{asset('images/bglove.jpg')}})">
+              </div>
+              <div class="carousel-item" style="background-image: url({{asset('images/bglove.jpg')}})">
+              </div>
+              <div class="carousel-item" style="background-image: url({{asset('images/bglove.jpg')}})">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+          <div class="bs-example mt-3">
+            {{-- <div class="alert alert-info alert-dismissible fade show">
+                <strong>Note!</strong> Open the output in a new blank tab (click the arrow button next to "Show Output") and resize the browser window to see how responsive videos work.
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+            </div> --}}
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/YE7VzlLtp-4"></iframe>
+            </div>
+            {{-- <h2 class="mt-4">Video with 4:3 aspect ratio</h2>
+            <div class="embed-responsive embed-responsive-4by3">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/YE7VzlLtp-4"></iframe>
+            </div> --}}
+        </div>        
     </div>
     <div class="content"></div>
 <script src="{{ asset('js/engpro/engpro.js') }}"></script>
