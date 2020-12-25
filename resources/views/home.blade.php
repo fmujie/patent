657db39e959fc610c87d82adf97741821b985222
fmujie <1935377905@qq.com>
2020-12-25 18:48:56 +0800

    delete nick

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Dear {{ $nick_name }}, you are logged in!
+                    You are logged in!
                 </div>
             </div>
         </div>
