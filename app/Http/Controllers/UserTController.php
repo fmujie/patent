c5ceda14bf495eb46ff0931583e64283469c4789
fmujie <1935377905@qq.com>
2020-12-25 19:11:57 +0800

    git mdified class name

--- app/Http/Controllers/UserTController.php
+++ app/Http/Controllers/UserTController.php
@@ -14,7 +14,7 @@ use Spatie\Permission\Models\Permission;
 //Enables us to output flash messaging
 use Session;
 
-class UserController extends Controller {
+class UserTController extends Controller {
 
     public function __construct() {
         $this->middleware(['auth', 'isAdmin']); //isAdmin middleware lets only users with a //specific permission permission to access these resources
